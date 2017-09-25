319db082964a06fd1810aade4712cba97d817e2b
Thiago@Oxossi <tsgouvea@cshl.edu>
2017-09-25 16:15:25 -0400

    Finished checking. It successfully converted, sorted, and opened mountainview. Attention: removed sample rate argumento from call

--- EvaluateClustering.m
+++ EvaluateClustering.m
@@ -1,20 +1,20 @@
 %% Script to analyse the data in mountainview
 
 %%%% USER %%%%%%%%%
-view_tetrode=13; %which tetrode to look at
-Animal = 'P37';
-Date = '2016-10-26';%for multiple sessions, Animals must be of same length
+view_tetrode=8; %which tetrode to look at
+Animal = 'TG020';
+Date = '20170921';%for multiple sessions, Animals must be of same length
 %%%%%%%%%%%%%%%%%%
 
 %%%%% PATHS %%%%%%%
-serverpathbase =  '/media/confidence/Data/';% source path to nlx files
-datapathbase = '/hdd/Data/Paul/'; %where to store mda files (big files). recommend HDD.
-sortingpathbase = '/home/hoodoo/mountainsort/'; %where to store mountainlab sorting results (small(er) files). recommend SSD.
+serverpathbase =  '/media/thiagoatserver/Neurodata/Spikegadgets';% source path to original recording files
+datapathbase = '/home/thiago/Neurodata/Spikegadgets'; %where to store mda files (big files). recommend HDD.
+sortingpathbase = '/shorthand/Neurodata/mountainsort/'; %where to store mountainlab sorting results (small(er) files). recommend SSD.
 %%%%%%%%%%%%%%%%%%
 
 %%%% RUN %%%%%%%%%
 params=struct('basepath',sortingpathbase,'animal',Animal,'date',Date,'tetrode',view_tetrode,...
-    'metrics','cluster_metrics_annotated.json','session',2);
+    'metrics','cluster_metrics_annotated.json','session',1);
 % ALL MOUNTAINVIEW
  start_mountainview(params);
 %%%%%%%%%%%%%%%%%%
